4ac50ba957e8c4ea186555ee0b6d5d2979678faf
Mateus <mateuscarvalhodev@outlook.com>
2023-05-20 17:14:27 -0300

    listando e paginando api

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -5,13 +5,29 @@ import { api } from './api/api';
 const Dashboard = () => {
 
   const [pokemon, setPokemon] = useState([]);
+  const [page, setPage] = useState(0);
+  const [isDisableNext, setIsDisableNext] = useState(false);
+  const [isDisablePrevious, setIsDisablePrevious] = useState(true);
 
   useEffect(() => {
-    api.get('/pokemon').then((response) => {
+    api.get('/pokemon', {
+      params: {
+        limit: 20,
+        offset: 20 * page
+      }
+    }).then((response) => {
+      
       console.log(response);
-      setPokemon(response.data.results);
+      const {next, previous, results} = response.data;
+      console.log(next, previous);
+      setIsDisableNext(!next);
+      setIsDisablePrevious(!previous);
+      setPokemon(results);
+      
     })
-  }, [])
+  }, [page]);
+
+  console.log('teste');
   return (
     <>
       <div className='flex flex-row gap-4 justify-center p-4'>
@@ -19,6 +35,18 @@ const Dashboard = () => {
         <Card />
         <Card />
         <Card />
+      </div>
+      <div>
+        <button
+         className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'
+         onClick={() => setPage(page -1)}
+         disabled={isDisablePrevious}
+         >Previous</button>
+        <button
+        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'
+         onClick={() => setPage(page +1)}
+         disabled={isDisableNext}
+         >Next</button>
       </div>
       <ul>
         {

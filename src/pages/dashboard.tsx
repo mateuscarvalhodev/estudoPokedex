3f6db34d3e9467e14fe0dfda6d1d85ff6a47f5f3
Mateus <mateuscarvalhodev@outlook.com>
2023-05-20 16:22:36 -0300

    api pokemon chamada

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,17 @@
 import Card from '@/Components/Card';
+import { useEffect, useState } from 'react';
+import { api } from './api/api';
 
 const Dashboard = () => {
+
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    api.get('/pokemon').then((response) => {
+      console.log(response);
+      setPokemon(response.data.results);
+    })
+  }, [])
   return (
     <>
       <div className='flex flex-row gap-4 justify-center p-4'>
@@ -9,6 +20,13 @@ const Dashboard = () => {
         <Card />
         <Card />
       </div>
+      <ul>
+        {
+          pokemon.map(item =>
+            <li key={item.name}>{item.name}</li>
+          )
+        }
+      </ul>
     </>
   );
 };

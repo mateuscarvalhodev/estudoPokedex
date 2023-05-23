34aea788b1a4d59a8a5ce0fdd60b22b0526014f3
Mateus <mateuscarvalhodev@outlook.com>
2023-05-23 08:11:47 -0300

    testes

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,24 +1,31 @@
+/* eslint-disable @next/next/no-img-element */
+// eslint-disable-next-line @next/next/no-img-element
 import Image from "next/image";
-import charmeleon from '@/assets/img/charmeleon.png';
-const Card = () => {
+import { ReactElement } from "react";
+import { IPokemons } from "@/pages/dashboard";
+
+
+
+const Card = ({ name, image, index, types }: IPokemons): ReactElement => {
   return (
     <>
       <div className="border-solid border-2 border-sky-500 max-w-sm rounded overflow-hidden shadow-lg">
-        <Image
-          src={charmeleon}
-          alt='pokemonX'
-          width={150}
-          height={150}
+        <img
+          src={image}
+          alt={name}
+          className="mx-auto"
+          width={100}
+          height={100}
         />
         <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Charmeleon</div>
+          <div className="font-bold text-xl mb-2">{index}{name}</div>
           <p className="text-gray-700 text-base">
-          Charmeleon impiedosamente destrói seus inimigos usando suas garras afiadas. Se ele encontrar um adversário forte, verifica-se agressivo. Neste estado animado, a chama na ponta da cauda se inflama com uma cor branca azulada.
+            {name}
           </p>
         </div>
         <div className="px-6 pt-4 pb-2">
-          <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:border-sky-500 hover:border-2'>#Fire</span>
-          <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:border-sky-500 hover:border-2'>#Normal</span>
+          <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:border-sky-500'>{types}</span>
+          <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:border-sky-500'>{name}</span>
         </div>
       </div>
     </>
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,9 +2,19 @@ import Card from '@/Components/Card';
 import { useEffect, useState } from 'react';
 import { api } from './api/api';
 
+
+export interface IPokemons {
+  sprites: any;
+  name: string;
+  image: string;
+  index: number;
+  types: Array<{ type: { name: string } }>
+}
 const Dashboard = () => {
 
+
   const [pokemon, setPokemon] = useState([]);
+  const [pokemonInfo, setPokemonInfo] = useState([]);
   const [page, setPage] = useState(0);
   const [isDisableNext, setIsDisableNext] = useState(false);
   const [isDisablePrevious, setIsDisablePrevious] = useState(true);
@@ -16,47 +26,61 @@ const Dashboard = () => {
         offset: 20 * page
       }
     }).then((response) => {
-      
+
       console.log(response);
-      const {next, previous, results} = response.data;
+      const { next, previous, results } = response.data;
       console.log(next, previous);
       setIsDisableNext(!next);
       setIsDisablePrevious(!previous);
       setPokemon(results);
-      
+
     })
   }, [page]);
 
-  console.log('teste');
+  useEffect(() => {
+    const loadPokemonInfo = async () => {
+      const loadedPokemonInfo = await Promise.all(
+        pokemon.map((p: { name: string, url?: string }) =>
+          api.get(`pokemon/${p.name}`)
+        )
+      );
+      setPokemonInfo(loadedPokemonInfo.map((response) => response.data));
+    };
+
+    loadPokemonInfo();
+  }, [pokemon]);
+  console.log(pokemonInfo);
   return (
     <>
-      <div className='flex flex-row gap-4 justify-center p-4'>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <div className='flex flex-wrap flex-row gap-4 justify-center p-4'>
+        {
+          pokemon.map((item: IPokemons, index: number) =>
+            <Card
+              key={item.index}
+              name={item.name}
+              index={item.index}
+              // image={item.sprites.front_default}
+              types={item.types}
+            // image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${(page * 20) + (index + 1)}.png`}
+            />
+          )
+        }
       </div>
       <div>
         <button
-         className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'
-         onClick={() => setPage(page -1)}
-         disabled={isDisablePrevious}
-         >Previous</button>
+          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full disabled:opacity-30'
+          onClick={() => setPage(page - 1)}
+          disabled={isDisablePrevious}
+        >Previous</button>
         <button
-        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'
-         onClick={() => setPage(page +1)}
-         disabled={isDisableNext}
-         >Next</button>
+          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full '
+          onClick={() => setPage(page + 1)}
+          disabled={isDisableNext}
+        >Next</button>
       </div>
-      <ul>
-        {
-          pokemon.map(item =>
-            <li key={item.name}>{item.name}</li>
-          )
-        }
-      </ul>
     </>
   );
 };
+// console.log(item.name);
 
 export default Dashboard;
